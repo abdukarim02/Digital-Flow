d363c1a464ea4aa29a00d40beeb6675951516ce3
abdukarim <kaumijabdukarim@gmail.com>
2025-01-28 13:09:33 +0500

    123

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-// vite.config.js
 export default defineConfig({
   base: '/Digital-Flow/', 
   plugins: [
